4151bd2efe41835a4c9354438ed64c10ed30ea35
M-A-Demir <m.a.demir1805@gmail.com>
2025-04-04 22:39:20 +0000

    Added (correct) license info to top of all files

--- 015_Memory_Profiling_LSQR_CGLS/Scripts/memory-sandstone.py
+++ 015_Memory_Profiling_LSQR_CGLS/Scripts/memory-sandstone.py
@@ -1,6 +1,22 @@
-import sys
+#  Copyright 2025 United Kingdom Research and Innovation
+#  Copyright 2025 The University of Manchester
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+# Authors:
+# Mariam Demir, SCD STFC
 
-import psutil
+import sys
 sys.path.append("./Algorithms")
 
 # from LSQR import *
@@ -27,7 +43,8 @@ from cil.plugins.astra import ProjectionOperator
 
 # Third-party imports
 import scipy
-import numpy as np    
+import numpy as np 
+import psutil   
 import os
 import argparse
 from threading import Event, Thread
